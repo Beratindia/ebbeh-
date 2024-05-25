7aa2c12c818e5883975a8586ce9973c919ac02f8
Beratindia <144980393+Beratindia@users.noreply.github.com>
2024-05-25 18:55:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function displayText() {
+    var text = document.getElementById("textField");
+    text.style.display = "block";
+  }
+let Username;
+let password;
+document.getElementById("btn").onclick = function(){
+ Username = document.getElementById("username").value;
+ document.getElementById("h1").textContent = "hewwo ebbeh <3";
+}
